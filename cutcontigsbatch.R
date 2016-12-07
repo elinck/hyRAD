ca902a8de953de06ebd5ff22347ab342d2661a7e
elinck <elinck@uw.edu>
2016-12-07 10:27:31 -0700

    add headed

--- cutcontigsbatch.R
+++ cutcontigsbatch.R
@@ -1,4 +1,9 @@
-
+#### cutcontigsbatch: an R function for removing specific contigs from multiple fasta file  ####
+# credit to R. Harris (https://github.com/rebzzy/) for the lapply() solution
+# useage: cutcontigsbatch(directory, contigs) where
+#         directory = path to directory that only contains fasta files of interest
+#         contigs = text file with contig names of interest, 1 per row (contigs <-readLines("test_contigs.txt"))
+# notes: use extractcontigIDs.R to generate contigs input text file from BLAST results (e.g., to remove contamination)
 
 cutcontigsbatch <- function(directory, contigs){
   files <- list.files(directory, pattern = ".fasta")
